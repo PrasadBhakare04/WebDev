12532aa46dedf912dfca0c7e96cc318f8e2d3333
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-04-02 10:13:09 -0700

    React State Basic

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -3,18 +3,37 @@ import './App.css'
 import Clicker from './ClickerExercise/Clicker'
 import ClickerForm from './ClickerExercise/ClickerForm'
 import Form from './ClickerExercise/Form'
+import ColorBox from './State/ColorBoxExercise/ColorBox'
+import ColorBoxGrid from './State/ColorBoxExercise/ColorBoxGrid'
+import Counter from './State/Counter'
+import Toggler from './State/Toggler'
 
 function App() {
   const data = [
     {id : 1, name : "Ocean View Villa", price : 2900},
     {id : 2, name : "river bed Hut", price : 3000}
   ]
+  const colors = [
+    "#FF5733",
+    "#33FF57",
+    "#3357FF",
+    "#FF33A1",
+    "#FFD700",
+    "#8A2BE2",
+    "#00FFFF",
+    "#FF4500",
+    "#40E0D0",
+    "#ADFF2F"
+]
   return (
     <>
       {/* <RentingList List = {data}/> */}
-      <Clicker/>
+      {/* <Clicker/>
       <Form/>
-      <ClickerForm message="Please Click Me again"/>
+      <ClickerForm message="Please Click Me again"/> */}
+      {/* <Counter/> */}
+      {/* <Toggler/> */}
+      <ColorBoxGrid colors = {colors}/>
     </>
   )
 }
