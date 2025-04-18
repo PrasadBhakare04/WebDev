310f93f949b93aad0c93259c466c7be002f6c77c
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-04-18 18:10:22 +0530

    Todo List using material ui and React

---
+++ React/react-todos/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline'
+
+function App() {
+  return (
+    <>
+     <CssBaseline/>
+     <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
