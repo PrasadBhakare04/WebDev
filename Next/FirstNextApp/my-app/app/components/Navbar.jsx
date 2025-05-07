25db0f3b19d61b0779a49eed6ea49f8ab882e5ba
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-05-07 15:11:51 +0530

    Next Js first app and Server, Client Components

---
+++ Next/FirstNextApp/my-app/app/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return <nav>
+        <h1>This is navbar</h1>
+         <li style={{listStyle:"none", display : "inline-block", padding : "0px 200px 0px 0px"}}>Home</li>
+         <li style={{listStyle:"none", display : "inline-block"}}>About</li>
+    </nav>
+}
---
+++ Next/FirstNextApp/my-app/app/components/RandomComponent.jsx
@@ -0,0 +1,7 @@
+"use client"
+export default function RandomComponent(){
+    console.log("Hello world")
+    return <div>
+        This is random component
+    </div>
+}
