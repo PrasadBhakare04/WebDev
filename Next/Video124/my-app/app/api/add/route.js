13b28ab4bc2e07fd1e28134c398f1431ae6ea1cc
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-05-10 15:32:14 +0530

    Post request Demo adv

--- Next/Video124/my-app/app/api/add/route.js
+++ Next/Video124/my-app/app/api/add/route.js
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     const data = await request.json();
     console.log(data);
-    return NextResponse.json({message : "Succcess", data : "the data"})
+    return NextResponse.json({message : "Succcess", data : data})
 }
--- Next/Video124/my-app/app/page.js
+++ Next/Video124/my-app/app/page.js
@@ -1,5 +1,9 @@
 "use client"
+
+import { useState } from "react";
+
 export default function Home() {
+  const [res, setRes] = useState();
   const data = { name : "Prasad", role : "Developer"};
   async function handleClick(){
     const req = await fetch('/api/add', {method : "POST", headers : {
@@ -7,11 +11,14 @@ export default function Home() {
     },
     body : JSON.stringify(data),
   })
-  let res = await req.json();
-  console.log(res)  
+  let response = await req.json();
+  setRes(response);
+  console.log(res)
 }
   return <>
+  
   <button onClick={handleClick}>Click Me</button>
   <div>Handling API Requests</div>
+  {res ? <div>{res.message}-{res.data.name}-{res.data.role}</div> : null}
   </>
 }
